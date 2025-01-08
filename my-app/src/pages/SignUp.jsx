7c3ddb9151517f450d08d0b1cdf49090c441d1ef
Amritha <169130861+Amruthapallavi@users.noreply.github.com>
2025-01-08 13:11:19 +0530

    complete signin

--- my-app/src/pages/SignUp.jsx
+++ my-app/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 
 function SignUp() {
 
@@ -7,6 +7,7 @@ function SignUp() {
   const [error,setError]=useState(false);
   const [loading,setLoading]=useState(false);
 
+  const navigate= useNavigate();
 
   const handleChange=(e)=>{
       
@@ -33,7 +34,7 @@ function SignUp() {
         setError(true);
           return;
         }
-
+        navigate('/sign-in')
       } catch (error) {
       setLoading(false);
       setError(true)
